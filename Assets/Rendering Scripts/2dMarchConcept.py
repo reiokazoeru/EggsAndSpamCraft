cc9af45bfa62486e9f9fc945f56723ef4c1c3ae3
reiokazoeru <reiokazoeru@gmail.com>
2021-10-20 10:56:09 -0400

    added pyopengl

--- Assets/Rendering Scripts/2dMarchConcept.py
+++ Assets/Rendering Scripts/2dMarchConcept.py
@@ -1,6 +1,6 @@
 from math import *
 import pygame
-from pygame.draw import circle
+from pygame.draw import circle, rect
 from pygame.locals import *
 """
 so for given inital point (x,y) at angel r, calculate distance to all objects in scene,
@@ -8,27 +8,30 @@ then draw cirle (just for vis) and extend line at angle r for shortest distance
 repeat until line length is less than given threshold
 rectangle eq
 """
+def tupleSub(t1:tuple,t2:tuple):
+    return tuple(map(lambda i, j: i - j, t1, t2))
+def tupleAbs(t1:tuple):
+    return (abs(t1[0]),abs(t1[1]))
 
-
-def length(p1): #use pythagoras to find line length from point offest from (0,0)
+def length(p1:tuple): #use pythagoras to find line length from point offest from (0,0)
     return sqrt(p1[0]*p1[0]+p1[1]*p1[1])
-def lineLength(p1,p2): #use length function to calc distance from p1 to p2
+def lineLength(p1:tuple,p2:tuple): #use length function to calc distance from p1 to p2
     return length((p1[0]-p2[0],p1[1]-p2[1]))
 
-def signedDistanceToCircle(p,center,radius:float):
-    return length(center-p)-radius
-def signedDistanceToRect(p,center,size):
-    offset = abs(p-center) - size
-    unsignedDist = length(max(offset,0))
-    dstInsideBox = max(min(offset,0))
+def signedDistanceToCircle(p:tuple,center:tuple,radius:float):
+    return lineLength(center,p)-radius
+def signedDistanceToRect(p:tuple,center:tuple,size:tuple):
+    offset = tupleSub(tupleAbs(tupleSub(p,center)),size)
+    unsignedDist = length(max(offset,(0,0)))
+    dstInsideBox = max(min(offset,(0,0)))
     return unsignedDist + dstInsideBox
 
 
 screenSize = ((640,240))
-mousePos = (0,0)
+mousePos = (0,0)    
 circleRadius = 0
-testCirclePos = (640/2,240/2)
-testCircleRadius = 5.0
+testPos = (640/2,240/2)
+testRadius = 20.0
 pygame.init()
 screen = pygame.display.set_mode(screenSize)
 runBool = True
@@ -38,11 +41,19 @@ while runBool:
             runBool = False
         elif event.type == MOUSEMOTION:
             mousePos= event.pos
-            circleRadius = signedDistanceToCircle(mousePos,testCirclePos,testCircleRadius)
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                testRadius +=5
+            elif event.button == 3:
+                testRadius -=5
+    #vars to update on frame advance
+    testRect = Rect(testPos[0]-testRadius/2,testPos[1]-testRadius/2,testRadius*2,testRadius*2)
+    circleRadius = signedDistanceToRect(mousePos,testRect.center,(testRadius,testRadius))
     #items to put on screen
     screen.fill((0,0,0))
     pygame.draw.circle(screen,(255,255,255),mousePos,circleRadius)
-    pygame.draw.circle(screen,(255,0,0),testCirclePos,testCircleRadius)
+    pygame.draw.rect(screen,(255,0,0),testRect)
+    #pygame.draw.circle(screen,(255,0,0),testPos,testRadius)
     #update screen
     pygame.display.update()
 pygame.quit()

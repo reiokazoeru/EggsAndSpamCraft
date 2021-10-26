6e8a504d457455b1aa21059e7721613a708d7cf0
reiokazoeru <reiokazoeru@gmail.com>
2021-10-26 09:33:18 -0400

    fully wrote rectangle march prog

--- Assets/Rendering Scripts/signedRectConcept.py
+++ Assets/Rendering Scripts/signedRectConcept.py
@@ -14,21 +14,11 @@ def lineLength(p1:tuple,p2:tuple): #use length function to calc distance from p1
     return length((p1[0]-p2[0],p1[1]-p2[1]))
 
 def goodSignedDistanceToRect(rect:Rect,target:tuple):
-    offset = tupleSub(target,rect.center)
+    offset = tupleAbs(tupleSub(target,rect.center))
     f = (rect.right-rect.centerx) #x size /2
-    g = (rect.top-rect.centery) #y size /2
-    rectTriSlope =g/f
-    genSlope = 0
-    if offset[0] != 0:
-        genSlope =offset[1]/offset[0]
-    if genSlope > rectTriSlope and genSlope < -1 * rectTriSlope:
-        dstToEdge = sqrt(f*f + (g*genSlope)*(g*genSlope))
-    elif genSlope != 0:
-        dstToEdge = sqrt(g*g + (f*(1/genSlope))*(f*(1/genSlope))) #doesnt work
-    else:
-        dstToEdge = g
-    unsignedLength = lineLength(target,rect.center)
-    return str(dstToEdge)+"=dstToEdge, "+str(unsignedLength)+"=unsigned, "+str(unsignedLength-dstToEdge)+"=signedDst"
+    g = -1 * (rect.top-rect.centery) #y size /2
+    signedDst = sqrt(max(0,(offset[0]-f))*max(0,(offset[0]-f))+max(0,(offset[1]-g))*max(0,(offset[1]-g)))
+    return signedDst
 pygame.init()
 screenSize = (640,240)
 screen = pygame.display.set_mode(screenSize)
@@ -47,9 +37,7 @@ while runBool:
             caption = str(lineLength(testRect.center,mousePos))
     screen.fill((0,0,0))
     pygame.draw.rect(screen,(255,0,0),testRect)
-    pygame.draw.line(screen,(255,255,255),testRect.center,mousePos,5)
-    pygame.draw.line(screen,(0,200,200),(mousePos[0],testRect.centery),mousePos,3)
-    pygame.draw.line(screen,(0,200,200),(testRect.centerx,mousePos[1]),mousePos,3)
+    pygame.draw.circle(screen,(255,255,255),mousePos,goodSignedDistanceToRect(testRect,mousePos))
     pygame.display.set_caption(str(goodSignedDistanceToRect(testRect,mousePos)))
     pygame.display.update()
 pygame.quit()

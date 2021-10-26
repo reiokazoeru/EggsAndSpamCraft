ee41319eebc70c9db29a904458fa28a6b308e572
reiokazoeru <reiokazoeru@gmail.com>
2021-10-26 12:26:58 -0400

    began demo

--- Assets/Rendering Scripts/signedRectConcept.py
+++ Assets/Rendering Scripts/signedRectConcept.py
@@ -1,7 +1,24 @@
 from math import *
 import pygame
-from pygame.draw import rect
+from pygame.draw import circle
 from pygame.locals import *
+class Shape():
+    #a general shape class to make life easier when dealing with many @ once
+    def __init__(self,type:str,pos:tuple,size,color=(255,255,255)) -> None:
+        self.type = type
+        self.pos = pos
+        self.size = size
+        self.color = color
+    def draw(self,screen): #draw the shape to the screen
+        if self.type == "circle":
+            pygame.draw.circle(screen,self.color,self.pos,self.size)
+        elif self.type == "rectangle":
+            pygame.draw.rect(screen,self.color,(self.pos,self.size))
+    def signedDst(self,target:tuple):
+        if self.type == "circle":
+            return signedDstCircle(target,self.pos,self.size)
+        elif self.type == "rectangle":
+            return signedDstRect(((self.pos),(self.size)),target)
 
 def tupleSub(t1:tuple,t2:tuple):
     return (t1[0]-t2[0],t1[1]-t2[1])
@@ -13,31 +30,52 @@ def length(p1:tuple): #use pythagoras to find line length from point offest from
 def lineLength(p1:tuple,p2:tuple): #use length function to calc distance from p1 to p2
     return length((p1[0]-p2[0],p1[1]-p2[1]))
 
-def goodSignedDistanceToRect(rect:Rect,target:tuple):
-    offset = tupleAbs(tupleSub(target,rect.center))
-    f = (rect.right-rect.centerx) #x size /2
-    g = -1 * (rect.top-rect.centery) #y size /2
+def signedDstRect(rectPos,RectSize,target:tuple):
+    offset = tupleAbs(tupleSub(target,rectPos))
+    f = (RectSize[0]/2) #x size /2
+    g = -1 * (RectSize[1]/2) #y size /2
     signedDst = sqrt(max(0,(offset[0]-f))*max(0,(offset[0]-f))+max(0,(offset[1]-g))*max(0,(offset[1]-g)))
     return signedDst
+def signedDstCircle(p:tuple,center:tuple,radius:float):
+    return lineLength(center,p)-radius
+def signedDstFList(list,target:tuple):
+    shortest = float('inf')
+    for i in list:
+        shortest = min(shortest,i.signedDst(target))
+    return shortest            
+def drawList(list:list,screen):
+    for i in list:
+        i.draw()
 pygame.init()
-screenSize = (640,240)
-screen = pygame.display.set_mode(screenSize)
-testSize = (320,120)
-testPos = ((screenSize[0]/2 - testSize[0]/2),(screenSize[1]/2 - testSize[1]/2)) # centers
-testRect = Rect(testPos[0],testPos[1],testSize[0],testSize[1])
-caption = "null"
-runBool = True
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+
 mousePos = (0,0)
+
+screenSize = (640,640)
+screen = pygame.display.set_mode(screenSize)
+runBool = True
+#shape vars
+idleShapeList= [Shape("circle",(120,420),30.0,BLUE),Shape("rectangle",(420,120),(20,10),RED)]
+activeShapeList= [Shape("circle",mousePos,0)]
+shapeList = [activeShapeList,idleShapeList]
 while runBool:
+    #event handler
     for event in pygame.event.get():
         if event.type == QUIT:
             runBool = False
         elif event.type == MOUSEMOTION:
             mousePos= event.pos
-            caption = str(lineLength(testRect.center,mousePos))
+    #shape constructer and updater
+    print(idleShapeList[1].type)
+    sDst = signedDstFList(idleShapeList,mousePos)
+    activeShapeList[0].pos = mousePos
+    activeShapeList[0].size = sDst
+    #screen drawer
     screen.fill((0,0,0))
-    pygame.draw.rect(screen,(255,0,0),testRect)
-    pygame.draw.circle(screen,(255,255,255),mousePos,goodSignedDistanceToRect(testRect,mousePos))
-    pygame.display.set_caption(str(goodSignedDistanceToRect(testRect,mousePos)))
+    drawList(shapeList)
     pygame.display.update()
 pygame.quit()

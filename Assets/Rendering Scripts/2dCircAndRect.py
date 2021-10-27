c9cced13ec52505eb17e4064715289847dab030a
reiokazoeru <reiokazoeru@gmail.com>
2021-10-27 11:30:53 -0400

    fixed opengl lib

--- Assets/Rendering Scripts/2dCircAndRect.py
+++ Assets/Rendering Scripts/2dCircAndRect.py
@@ -21,17 +21,19 @@ class Shape():
         elif self.type == "rectangle":
             return signedDstRect((self.pos),(self.size),target)
 
+
 def tupleSub(t1:tuple,t2:tuple):
     return (t1[0]-t2[0],t1[1]-t2[1])
 def tupleAbs(t1:tuple):
     return (abs(t1[0]),abs(t1[1]))
+
 
 def length(p1:tuple): #use pythagoras to find line length from point offest from (0,0)
     return sqrt(p1[0]*p1[0]+p1[1]*p1[1])
 def lineLength(p1:tuple,p2:tuple): #use length function to calc distance from p1 to p2
     return length((p1[0]-p2[0],p1[1]-p2[1]))
 
-def signedDstRect(rectPos,rectSize,target:tuple):
+def signedDstRect(rectPos,rectSize,target:tuple): #gen the signed distance to a rectangle while not on angle
     offset = tupleAbs(tupleSub(target,(rectPos[0]+rectSize[0]/2,rectPos[1]+rectSize[1]/2)))
     f = (rectSize[0]/2) #x size /2
     g = -1* (rectSize[1]/2) #y size /2
@@ -48,28 +50,35 @@ def drawList(list:list,screen):
     for l in list:
         for i in l:
             i.draw(screen)
+
+
+
 pygame.init()
+#colour constants
 WHITE = (255,255,255)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
-
-
+#mousePosInit
 mousePos = (0,0)
-
+#pre runtime stuff
 screenSize = (640,640)
 screen = pygame.display.set_mode(screenSize)
 runBool = True
-#shape vars
+#idle shape vars
 testCircle = Shape("circle",(120,420),30.0,BLUE)
 testRect = Shape("rectangle",(420,120),(80,80),RED)
 testRect1 = Shape("rectangle",(320,320),(80,80),RED)
+#shape lists
 idleShapeList= list()
 idleShapeList.append(testCircle)
 idleShapeList.append(testRect)
 idleShapeList.append(testRect1)
+#activeShapeVars
 activeShapeList= [Shape("circle",mousePos,0)]
+#main shape list
 shapeList = [activeShapeList,idleShapeList]
+#runtime
 while runBool:
     #event handler
     for event in pygame.event.get():
@@ -83,8 +92,8 @@ while runBool:
     sDst = signedDstFList(idleShapeList,mousePos)
     activeShapeList[0].pos = mousePos
     activeShapeList[0].size = sDst
-    #screen drawer
-    screen.fill((0,0,0))
-    drawList(shapeList,screen)
-    pygame.display.update()
+    #screen updater
+    screen.fill((0,0,0)) #add background to buffer
+    drawList(shapeList,screen) #add all shapes to buffer
+    pygame.display.update() #draw buffer
 pygame.quit()
--- Assets/Rendering Scripts/OpenGLtut.py
+++ Assets/Rendering Scripts/OpenGLtut.py
@@ -1,5 +1,25 @@
-import OpenGL
-import OpenGL.GL
-import OpenGL.GLUT
-import OpenGL.GLU
-#print("Library Imports Successful!")
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+window = 0                                             # glut window number
+width, height = 500, 400                               # window size
+
+def draw():                                            # ondraw is called all the time
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear the screen
+    glLoadIdentity()                                   # reset position
+   
+    # ToDo draw rectangle
+   
+    glutSwapBuffers()                                  # important for double buffering
+   
+
+# initialization
+glutInit()                                             # initialize glut
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+glutInitWindowSize(width, height)                      # set window size
+glutInitWindowPosition(0, 0)                           # set window position
+window = glutCreateWindow("gex")              # create window with title
+glutDisplayFunc(draw)                                  # set draw function callback
+glutIdleFunc(draw)                                     # draw all the time
+glutMainLoop()    

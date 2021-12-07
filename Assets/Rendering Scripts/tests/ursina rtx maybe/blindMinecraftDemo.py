58d4b99c602513d2f8a28d668425bb6d0a742513
reiokazoeru <reiokazoeru@gmail.com>
2021-12-07 11:45:06 -0500

    commented all written mesh generation script

--- Assets/Rendering Scripts/tests/ursina rtx maybe/blindMinecraftDemo.py
+++ Assets/Rendering Scripts/tests/ursina rtx maybe/blindMinecraftDemo.py
@@ -18,7 +18,7 @@ triSpace = [[[0 for i in range(worldSize[2])] for i in range(worldSize[1])] for 
 
 worldSpace[0][0] = [1 for i in range(worldSize[2])]
 
-def genCubeVerts(c):
+def genCubeVerts(c): # Generate all 8 potential points of a cube
     return [
         (c[0],c[1],c[2]),
         (c[0]+1,c[1],c[2]),
@@ -29,46 +29,46 @@ def genCubeVerts(c):
         (c[0]+1,c[1],c[2]+1),
         (c[0],c[1],c[2]+1)
     ]
-def genWorldVerts(wSp):
+def genWorldVerts(wSp): #make a grid of all points in mesh
     mainVert = [[[0 for i in range(vertSize[2])] for i in range(vertSize[1])] for i in range(vertSize[0])]
     # iterate for every 3d point
     for x in range(wSp):
         for y in range(wSp[x]):
             for z,block in enumerate(wSp[x][y]):
-                xBorderPoints = [(x+1,y,z),(x+1,y+1,z),(x+1,y,z+1),(x+1,y+1,z+1)]
-                yBorderPoints = [(x,y+1,z),(x+1,y+1,z),(x,y+1,z+1),(x+1,y+1,z+1)]
-                zBorderPoints = [(x+1,y,z+1),(x+1,y+1,z+1),(x,y+1,z+1),(x+1,y+1,z+1)]
-                if block != 0:
-                    tempVert = genCubeVerts((x,y,z))
+                if block != 0: # check for a block at the given position
+                    tempVert = genCubeVerts((x,y,z)) # find all 8 potential verts of the block
+                    # remove vertices that border another block
                     if wSp[x+1][y][z] != 0:
-                        for p in xBorderPoints:
+                        for p in [(x+1,y,z),(x+1,y+1,z),(x+1,y,z+1),(x+1,y+1,z+1)]:
                             tempVert.remove(p)
                     if wSp[x][y+1][z] != 0:
-                        for p in yBorderPoints:
+                        for p in [(x,y+1,z),(x+1,y+1,z),(x,y+1,z+1),(x+1,y+1,z+1)]:
                             if p in tempVert:
                                 tempVert.remove(p)
                     if wSp[x][y][z+1] != 0:
-                        for p in zBorderPoints:
+                        for p in [(x+1,y,z+1),(x+1,y+1,z+1),(x,y+1,z+1),(x+1,y+1,z+1)]:
                             if p in tempVert:
                                 tempVert.remove(p)
+                    # place good verts on grid of all potential points
                     for p in tempVert:
                         mainVert[p[0]][p[1]][p[2]] = 1
-    return mainVert
+    return mainVert # return the vert grid
 
-def genWorldVertsAndTris(wSp,vSp):
-    vertOut = []
-    triOut = []
+def genWorldVertsAndTris(wSp,vSp): # make a usable tuples of verts and tris from block grid and from vert grid
+    vertOut = [] # output var for verts
+    triOut = [] # output var for tris
     # iterate for every 3d point
     for x in range(vSp):
         for y in range(vSp[x]):
             for z,vert in enumerate(vSp[x][y]):
+                # check if there is a point on the grid, if so put it on the list of verts
                 if vert == 1:
-                    vertOut.append((x,y,z))
+                    vertOut.append((x,y,z)) 
     for x in range(wSp):
         for y in range(wSp[x]):
             for z,block in enumerate(wSp[x][y]):
+                # check every spot for a block
                 if block != 0:
-                    
                     # check if tris are needed on that face by checking for a border block
                     # find verts on that face
                     # get index of the face points in the main vertex list
@@ -103,7 +103,7 @@ def genWorldVertsAndTris(wSp,vSp):
                         nzBPi = [vertOut.index(i) for i in nzBP]
                         triOut.append((nzBPi[1],nzBPi[2],nzBPi[0]))
                         triOut.append((nzBPi[2],nzBPi[3],nzBPi[0]))
-    return tuple(vertOut), tuple(triOut)
+    return tuple(vertOut), tuple(triOut) # return the list of verts and tris as tuples as per the mesh function
 
 def update():
     pass
